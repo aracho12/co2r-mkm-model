63d5a3ecc8f0c536f6d35f27edb93c3c0559f6bf
Ara Cho <43310503+aracho12@users.noreply.github.com>
2025-03-14 13:01:28 -0700

    .

--- generate_mkm_inputs.py
+++ generate_mkm_inputs.py
@@ -1,21 +1,21 @@
 from core.formation_energy_calculator import generate_formation_energies
 ts_dict = {
     'H-ele': {
-        'activation_energy': 0.80, #1.07 0.75는 Ga로 0.991 -> 1.141
+        'activation_energy': 0.80, 
         'direction': 'forward',
         'reference': 'This work',
         'reaction_step_id': 1,
         'ae': 'Ea'
     },
     'H2-ele': {
-        'activation_energy': 0.53, #0.76 -> Ga: 1.182 
+        'activation_energy': 0.76,
         'direction': 'forward',
         'reference': 'This work',
         'reaction_step_id': 2,
         'ae': 'Ea'
     },
     'CO2-H-ele': {
-        'activation_energy': 0.60, #원래 0.60
+        'activation_energy': 0.60, 
         'direction': 'forward',
         'reference': 'This work',
         'reaction_step_id': 32,
@@ -29,7 +29,7 @@ ts_dict = {
         'ae': 'Ga'
     },
     'H-CO2-ele': {
-        'activation_energy': 0.7, #0.9 -> 1.04
+        'activation_energy': 0.7, 
         'direction': 'forward',
         'reference': 'This work',
         'reaction_step_id': 34,
@@ -47,17 +47,8 @@ ts_dict = {
 correction_dict = {
     'COH': -0.15,
     'CO-H-ele': -0.1,
-    #'CO-H':-0.15,
-    'COH-H-ele': -0.15,
-    #'COH-H':-0.15,
-    #'OCCO': -0.1,
-    # 'COOH': -0.2,
-    #'COOH-H-ele': -0.1,
-    #'CO2-H-ele': -0.1,
-    'H':-0.1,
-    #'H-ele':-0.1,
-    #'H2-ele':-0.05,
 }
+
 # Example usage:
 DB_NAME = 'co2r-suncat.db'
 save_dir = '.'
@@ -68,7 +59,7 @@ if __name__ == "__main__":
         db_path=DB_NAME,
         metal='Cu',
         facets=['100','211'],
-        she_range=(-1.14, -0.40, 0.02),
+        she_range=(-2.00, -0.40, 0.02),
         if_efield=True,
         ts_dict=ts_dict,
         correction_dict=correction_dict,
