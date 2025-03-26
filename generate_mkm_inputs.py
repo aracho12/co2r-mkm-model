5943dee5a15f4477c2c493d99a86071ec62126de
Ara Cho <43310503+aracho12@users.noreply.github.com>
2025-03-26 12:32:45 -0700

    Update generate_mkm_inputs.py

--- generate_mkm_inputs.py
+++ generate_mkm_inputs.py
@@ -1,52 +1,54 @@
 from core.formation_energy_calculator import generate_formation_energies
-ts_dict = {
-    'H-ele': {
-        'activation_energy': 0.80, 
-        'direction': 'forward',
-        'reference': 'This work',
-        'reaction_step_id': 1,
-        'ae': 'Ea'
-    },
-    'H2-ele': {
-        'activation_energy': 0.76,
-        'direction': 'forward',
-        'reference': 'This work',
-        'reaction_step_id': 2,
-        'ae': 'Ea'
-    },
-    'CO2-H-ele': {
-        'activation_energy': 0.60, 
-        'direction': 'forward',
-        'reference': 'This work',
-        'reaction_step_id': 32,
-        'ae': 'Ga'
-    },
-    'COOH-H-ele': {
-        'activation_energy': 0.40, 
-        'direction': 'forward',
-        'reference': 'This work',
-        'reaction_step_id': 33,
-        'ae': 'Ga'
-    },
-    'H-CO2-ele': {
-        'activation_energy': 0.7, 
-        'direction': 'forward',
-        'reference': 'This work',
-        'reaction_step_id': 34,
-        'ae': 'Ga'
-    },
-    'HCOO-H-ele': {
-        'activation_energy': 0.40, 
-        'direction': 'forward',
-        'reference': 'This work',
-        'reaction_step_id': 35,
-        'ae': 'Ga'
-    },
-}
+# ts_dict = {
+#     'H-ele': {
+#         'activation_energy': 0.80, 
+#         'direction': 'forward',
+#         'reference': 'This work',
+#         'reaction_step_id': 1,
+#         'ae': 'Ea'
+#     },
+#     'H2-ele': {
+#         'activation_energy': 0.76,
+#         'direction': 'forward',
+#         'reference': 'This work',
+#         'reaction_step_id': 2,
+#         'ae': 'Ea'
+#     },
+#     'CO2-H-ele': {
+#         'activation_energy': 0.60, 
+#         'direction': 'forward',
+#         'reference': 'This work',
+#         'reaction_step_id': 32,
+#         'ae': 'Ga'
+#     },
+#     'COOH-H-ele': {
+#         'activation_energy': 0.40, 
+#         'direction': 'forward',
+#         'reference': 'This work',
+#         'reaction_step_id': 33,
+#         'ae': 'Ga'
+#     },
+#     'H-CO2-ele': {
+#         'activation_energy': 0.7, 
+#         'direction': 'forward',
+#         'reference': 'This work',
+#         'reaction_step_id': 34,
+#         'ae': 'Ga'
+#     },
+#     'HCOO-H-ele': {
+#         'activation_energy': 0.40, 
+#         'direction': 'forward',
+#         'reference': 'This work',
+#         'reaction_step_id': 35,
+#         'ae': 'Ga'
+#     },
+# }
 
 correction_dict = {
     'COH': -0.15,
     'CO-H-ele': -0.1,
+    'H-ele': -0.26,
+    'H2-ele': -0.26,
 }
 
 # Example usage:
@@ -61,7 +63,7 @@ if __name__ == "__main__":
         facets=['100','211'],
         she_range=(-2.00, -0.40, 0.02),
         if_efield=True,
-        ts_dict=ts_dict,
+        #ts_dict=ts_dict,
         correction_dict=correction_dict,
         save_dir=save_dir
     )
